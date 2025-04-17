040115e1108d2fc002abce9d446cd2fb46fb7714
Roland Marvin <rolandmarvin007@hotmail.com>
2025-04-17 16:25:22 +0200

    Scrpit rotate

---
+++ Runtime/QuickRoteteMono.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Roland.QuickScrpit { 
+public class QuickRoteteMono : MonoBehaviour
+{
+    public Transform m_whatToRotale;
+    public float m_globalSpeedInAngle;
+    public Vector3 m_rotateAxis = Vector3.up;
+    public Space m_space = Space.Self;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+
+        transform.Rotate(m_rotateAxis, m_globalSpeedInAngle * deltaTime, m_space);
+    }
+    }
+}
